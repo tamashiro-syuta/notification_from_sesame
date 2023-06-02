6b5e3b2dba9fc3a993d0cef2b5d042977ca462c8
tamashiro-syuta <syuta5825@gmail.com>
2023-06-02 22:10:53 +0900

    🔥 動くか検証するためのコードを削除

--- src/middleware/sample.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const sampleMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log('next!!!!!!')
-
-  next()
-};
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { lock, sample, webhook } from './controllers/notifyAfterOperateSesameController';
 import { healthCheck } from './controllers/healthCheckController';
-import { sampleMiddleware } from './middleware/sample';
 import { isAuthenticated } from './middleware/isAuthenticated';
 import { authenticate } from './middleware/authenticate';
 
@@ -10,7 +9,6 @@ export const router = express.Router();
 router.use('/lock', isAuthenticated);
 router.use('/webhook', isAuthenticated);
 router.use('/webhook', authenticate);
-router.use('/sample', sampleMiddleware);
 
 router.get('/', healthCheck);
 router.get('/lock', lock);
